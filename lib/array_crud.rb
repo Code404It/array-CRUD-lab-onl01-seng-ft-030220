b531212881b43e2a3ca0e94eb64e9770faf09364
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-07 01:33:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
- array.push("Who")
+ array.push(element)
 end
 
 def add_element_to_start_of_array
-  array.unshift("Me") 
+  array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
